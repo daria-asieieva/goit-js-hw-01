5aab7eb67c6cad07e536da2f37811157f3358c23
Daria <dariaasieieva@gmail.com>
2024-08-21 23:40:14 +0200

    3

--- js/task-1.js
+++ js/task-1.js
@@ -6,6 +6,4 @@ function makeTransaction(quantity, pricePerDroid) {
 
 }
 
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000)); 
-console.log(makeTransaction(10, 500)); 
+ 
